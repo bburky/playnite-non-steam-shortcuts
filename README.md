8f0f446d20d5b0f91493684b1ab2b2b7ffc6e44c
Blake Burkhart <bburky@bburky.com>
2019-01-05 04:10:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Non-Steam Shortcut Extension for Playnite
+
+This extension will automatically creates non-Steam shortcuts for the currently selected games in Playnite. Multiple games can be highlighted to create multiple shortcuts at once. The game in Playnite will be modified to launch the game using Steam.
+
+Rerunning this extension is safe, it will update the shortcut with the information from the OtherAction titled "Launch without Steam".
+
+This effectively allows using the Steam overlay for any game in Playnite.
+
+## Installation
+
+1. [Download](https://github.com/bburky/playnite-non-steam-shortcuts/archive/master.zip) this extension
+2. Extract the extension into a new folder in your "Extensions" folder in your Playnite library.
+3. Update `SHORTCUT_VDF_PATH` at the top of `nonsteam.py` manually to be the location of your `shortcuts.vdf` file.
+4. Relaunch Playnite or select "Tools" → "Reload Scripts".
+
+You should have a new folder in your `Extensions` folder called `playnite-non-steam-shortcuts` containing `extension.yaml` and `nonsteam.py`.
+
+Select some games and chose "Extensions" → "Create non-Steam shortcuts for selected games" in the menu.
+
+## Sources used for shortcut.vdf reverse engineering
+
+*  https://github.com/CorporalQuesadilla/Steam-Shortcut-Manager/wiki/Steam-Shortcuts-Documentation
+*  https://github.com/tirish/steam-shortcut-editor/blob/master/lib/parser.js
+
+## Issues/TODO
+
+Currently I can't figure out how to allow configuring a script extension. Please update `SHORTCUT_VDF_PATH` at the top of `nonsteam.py` manually to be the location of your `shortcuts.vdf` file.
---
+++ nonsteam.py
@@ -0,0 +1,468 @@
+# Non-Steam Shortcuts
+
+# TODO: figure out how to make this configurable
+# Can prompt for a folder with PlayniteApi.Dialogs.SelectFolder()
+# But there is no way for an Extension to save configuration data?
+SHORTCUT_VDF_PATH = r"C:\Program Files (x86)\Steam\userdata\58546832\config\shortcuts.vdf"
+
+
+
+### crc_algorithms.py (Playnite won't import python files)
+
+#  pycrc -- parameterisable CRC calculation utility and C source code generator
+#
+#  Copyright (c) 2006-2013  Thomas Pircher  <tehpeh@gmx.net>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+#  IN THE SOFTWARE.
+
+
+"""
+CRC algorithms implemented in Python.
+If you want to study the Python implementation of the CRC routines, then this
+is a good place to start from.
+
+The algorithms Bit by Bit, Bit by Bit Fast and Table-Driven are implemented.
+
+This module can also be used as a library from within Python.
+
+Examples
+========
+
+This is an example use of the different algorithms:
+
+>>> from crc_algorithms import Crc
+>>>
+>>> crc = Crc(width = 16, poly = 0x8005,
+...           reflect_in = True, xor_in = 0x0000,
+...           reflect_out = True, xor_out = 0x0000)
+>>> print("0x%x" % crc.bit_by_bit("123456789"))
+>>> print("0x%x" % crc.bit_by_bit_fast("123456789"))
+>>> print("0x%x" % crc.table_driven("123456789"))
+"""
+
+# Class Crc
+###############################################################################
+class Crc(object):
+    """
+    A base class for CRC routines.
+    """
+
+    # Class constructor
+    ###############################################################################
+    def __init__(self, width, poly, reflect_in, xor_in, reflect_out, xor_out, table_idx_width = None):
+        """The Crc constructor.
+
+        The parameters are as follows:
+            width
+            poly
+            reflect_in
+            xor_in
+            reflect_out
+            xor_out
+        """
+        self.Width          = width
+        self.Poly           = poly
+        self.ReflectIn      = reflect_in
+        self.XorIn          = xor_in
+        self.ReflectOut     = reflect_out
+        self.XorOut         = xor_out
+        self.TableIdxWidth  = table_idx_width
+
+        self.MSB_Mask = 0x1 << (self.Width - 1)
+        self.Mask = ((self.MSB_Mask - 1) << 1) | 1
+        if self.TableIdxWidth != None:
+            self.TableWidth = 1 << self.TableIdxWidth
+        else:
+            self.TableIdxWidth = 8
+            self.TableWidth = 1 << self.TableIdxWidth
+
+        self.DirectInit = self.XorIn
+        self.NonDirectInit = self.__get_nondirect_init(self.XorIn)
+        if self.Width < 8:
+            self.CrcShift = 8 - self.Width
+        else:
+            self.CrcShift = 0
+
+
+    # function __get_nondirect_init
+    ###############################################################################
+    def __get_nondirect_init(self, init):
+        """
+        return the non-direct init if the direct algorithm has been selected.
+        """
+        crc = init
+        for i in range(self.Width):
+            bit = crc & 0x01
+            if bit:
+                crc^= self.Poly
+            crc >>= 1
+            if bit:
+                crc |= self.MSB_Mask
+        return crc & self.Mask
+
+
+    # function reflect
+    ###############################################################################
+    def reflect(self, data, width):
+        """
+        reflect a data word, i.e. reverts the bit order.
+        """
+        x = data & 0x01
+        for i in range(width - 1):
+            data >>= 1
+            x = (x << 1) | (data & 0x01)
+        return x
+
+
+    # function bit_by_bit
+    ###############################################################################
+    def bit_by_bit(self, in_str):
+        """
+        Classic simple and slow CRC implementation.  This function iterates bit
+        by bit over the augmented input message and returns the calculated CRC
+        value at the end.
+        """
+        register = self.NonDirectInit
+        for c in in_str:
+            octet = ord(c)
+            if self.ReflectIn:
+                octet = self.reflect(octet, 8)
+            for i in range(8):
+                topbit = register & self.MSB_Mask
+                register = ((register << 1) & self.Mask) | ((octet >> (7 - i)) & 0x01)
+                if topbit:
+                    register ^= self.Poly
+
+        for i in range(self.Width):
+            topbit = register & self.MSB_Mask
+            register = ((register << 1) & self.Mask)
+            if topbit:
+                register ^= self.Poly
+
+        if self.ReflectOut:
+            register = self.reflect(register, self.Width)
+        return register ^ self.XorOut
+
+
+    # function bit_by_bit_fast
+    ###############################################################################
+    def bit_by_bit_fast(self, in_str):
+        """
+        This is a slightly modified version of the bit-by-bit algorithm: it
+        does not need to loop over the augmented bits, i.e. the Width 0-bits
+        wich are appended to the input message in the bit-by-bit algorithm.
+        """
+        register = self.DirectInit
+        for c in in_str:
+            octet = ord(c)
+            if self.ReflectIn:
+                octet = self.reflect(octet, 8)
+            for i in range(8):
+                topbit = register & self.MSB_Mask
+                if octet & (0x80 >> i):
+                    topbit ^= self.MSB_Mask
+                register <<= 1
+                if topbit:
+                    register ^= self.Poly
+            register &= self.Mask
+        if self.ReflectOut:
+            register = self.reflect(register, self.Width)
+        return register ^ self.XorOut
+
+
+    # function gen_table
+    ###############################################################################
+    def gen_table(self):
+        """
+        This function generates the CRC table used for the table_driven CRC
+        algorithm.  The Python version cannot handle tables of an index width
+        other than 8.  See the generated C code for tables with different sizes
+        instead.
+        """
+        table_length = 1 << self.TableIdxWidth
+        tbl = [0] * table_length
+        for i in range(table_length):
+            register = i
+            if self.ReflectIn:
+                register = self.reflect(register, self.TableIdxWidth)
+            register = register << (self.Width - self.TableIdxWidth + self.CrcShift)
+            for j in range(self.TableIdxWidth):
+                if register & (self.MSB_Mask << self.CrcShift) != 0:
+                    register = (register << 1) ^ (self.Poly << self.CrcShift)
+                else:
+                    register = (register << 1)
+            if self.ReflectIn:
+                register = self.reflect(register >> self.CrcShift, self.Width) << self.CrcShift
+            tbl[i] = register & (self.Mask << self.CrcShift)
+        return tbl
+
+
+    # function table_driven
+    ###############################################################################
+    def table_driven(self, in_str):
+        """
+        The Standard table_driven CRC algorithm.
+        """
+        tbl = self.gen_table()
+
+        register = self.DirectInit << self.CrcShift
+        if not self.ReflectIn:
+            for c in in_str:
+                tblidx = ((register >> (self.Width - self.TableIdxWidth + self.CrcShift)) ^ ord(c)) & 0xff
+                register = ((register << (self.TableIdxWidth - self.CrcShift)) ^ tbl[tblidx]) & (self.Mask << self.CrcShift)
+            register = register >> self.CrcShift
+        else:
+            register = self.reflect(register, self.Width + self.CrcShift) << self.CrcShift
+            for c in in_str:
+                tblidx = ((register >> self.CrcShift) ^ ord(c)) & 0xff
+                register = ((register >> self.TableIdxWidth) ^ tbl[tblidx]) & (self.Mask << self.CrcShift)
+            register = self.reflect(register, self.Width + self.CrcShift) & self.Mask
+
+        if self.ReflectOut:
+            register = self.reflect(register, self.Width)
+        return register ^ self.XorOut
+
+#### End crc_algorithms
+
+import io
+import struct
+import shutil
+from collections import Counter
+from System.Collections.ObjectModel import ObservableCollection
+import traceback
+# import crc_algorithms
+
+
+SHORTCUT_DEFAULTS = {
+    "AllowOverlay": 1,
+    "AllowDesktopConfig": 1,
+    "ShortcutPath": "",
+    "IsHidden": 0,
+    "OpenVR": 0,
+    "tags": {},
+    "LastPlayTime": 0,
+    "Devkit": 0,
+    "DevkitGameID": "",
+}
+
+# Parse shortcuts.vdf
+
+def parse_object(stream):
+    k = parse_string(stream)
+    v = dict(iter(lambda: parse(stream), None))
+    return k, v
+
+def parse_int_value(stream):
+    k = parse_string(stream)
+    v, = struct.unpack("i", stream.read(4))
+    return k, v
+
+def parse_string_value(stream):
+    k = parse_string(stream)
+    v = parse_string(stream)
+    return k, v
+
+def parse_string(stream):
+    return "".join(iter(lambda: stream.read(1),"\x00"))
+
+parse_types = {
+    "\x00": parse_object,
+    "\x01": parse_string_value,
+    "\x02": parse_int_value,
+    "\x08": lambda stream: None,
+}
+
+def parse(stream):
+    data_type = stream.read(1)
+    return parse_types[data_type](stream)
+
+def parse_shortcuts(stream):
+    shortcuts = parse(stream)[1].values()
+    if Counter(s["AppName"] for s in shortcuts).most_common()[0][1] > 1:
+        raise "Duplicate AppNames in Steam shortcuts"
+    return {s["AppName"]: s for s in shortcuts}
+
+# Dump shortcuts.vdf
+
+def dump_object_value(stream, k, values):
+    stream.write("\x00")
+    stream.write(k)
+    stream.write("\x00")
+    for k, v in values.iteritems():
+        if isinstance(v, dict):
+            dump_object_value(stream, k, v)
+        elif isinstance(v, str):
+            dump_string_value(stream, k, v)
+        elif isinstance(v, int):
+            dump_int_value(stream, k, v)
+        elif isinstance(v, bool):
+            dump_int_value(stream, k, int(v))
+        else:
+            raise TypeError("Unrecognized type:", type(v))
+    stream.write("\x08")
+
+def dump_string_value(stream, k, v):
+    stream.write("\x01")
+    stream.write(k)
+    stream.write("\x00")
+    stream.write(v)
+    stream.write("\x00")
+
+def dump_int_value(stream, k, v):
+    stream.write("\x02")
+    stream.write(k)
+    stream.write("\x00")
+    stream.write(struct.pack("i", v))
+
+def dump_shortcuts(stream, shortcuts):
+    # Any string can be used as the key in shortcuts.vdf
+    # Originally it is a string of the index, but we use the AppName instead
+    dump_object_value(stream, "shortcuts", shortcuts)
+    stream.write("\x08")
+
+def steam_URL(shortcut):
+    # Comments by Scott Rice:
+    """
+    Calculates the filename for a given shortcut. This filename is a 64bit
+    integer, where the first 32bits are a CRC32 based off of the name and
+    target (with the added condition that the first bit is always high), and
+    the last 32bits are 0x02000000.
+    """
+    # This will seem really strange (where I got all of these values), but I
+    # got the xor_in and xor_out from disassembling the steamui library for
+    # OSX. The reflect_in, reflect_out, and poly I figured out via trial and
+    # error.
+    algorithm = Crc(width = 32, poly = 0x04C11DB7, reflect_in = True, xor_in = 0xffffffff, reflect_out = True, xor_out = 0xffffffff)
+    input_string = shortcut["exe"] + shortcut["AppName"]
+    top_32 = algorithm.bit_by_bit(input_string) | 0x80000000
+    full_64 = (top_32 << 32) | 0x02000000
+    return "steam://rungameid/" + str(full_64)
+
+def find_play_action(game):
+    """
+    Check if there is an existing OtherAction titled "Launch without Steam".
+
+    This will occur if the script is rerun on a game. We want to find the
+    original PlayAction to update the non-Steam shortcut.
+    """
+
+    if game.OtherActions:
+        for play_action in game.OtherActions:
+            if play_action.Name == "Launch without Steam":
+                return play_action
+    return game.PlayAction
+
+def non_steam_shortcuts():
+    games_updated = 0
+    games_new = 0
+    games_skipped = []
+    games_url = []
+
+    try:
+        shutil.copyfile(SHORTCUT_VDF_PATH, SHORTCUT_VDF_PATH+".bak")
+    except Exception as e:
+        PlayniteApi.Dialogs.ShowErrorMessage(
+            traceback.format_exc(),
+            "Error backing up shortcuts.vdf")
+        return
+
+    try:
+        with io.open(SHORTCUT_VDF_PATH, "rb") as f:
+            steam_shortcuts = parse_shortcuts(f)
+    except Exception as e:
+        PlayniteApi.Dialogs.ShowErrorMessage(
+            traceback.format_exc(),
+            "Error loading shortcuts.vdf")
+        return
+
+    for game in PlayniteApi.MainView.SelectedGames:
+        play_action = find_play_action(game)
+
+        # If a game somehow has no PlayAction, skip it
+        if not play_action:
+            games_skipped.append(game.Name)
+            continue
+
+        # If a game has a URL PlayAction, use it anyway but log it
+        if play_action.Type == GameActionType.URL:
+            games_url.append(game.Name)
+
+        # Create/Update Non-Steam shortcut
+        shortcut = {
+            "icon": game.Icon,
+            "exe": play_action.Path,
+            "StartDir": game.InstallDirectory,
+            "AppName": game.Name,
+            "LaunchOptions": play_action.Arguments or "",
+        }
+        if game.Name in steam_shortcuts:
+            games_updated += 1
+            steam_shortcuts[game.Name].update(shortcut)
+            shortcut = steam_shortcuts[game.Name]
+        else:
+            games_new += 1
+            shortcut.update(SHORTCUT_DEFAULTS)
+            steam_shortcuts[game.Name] = shortcut
+
+        # Update Playnite actions
+        # Only run once, don't create duplicate OtherActions
+        if play_action == game.PlayAction:
+            old_action = game.PlayAction
+            steam_action = GameAction(
+                Name="Non-Steam Steam Shortcut",
+                Type=GameActionType.URL,
+                Path=steam_URL(shortcut),
+                IsHandledByPlugin=False,
+            )
+            game.PlayAction = steam_action
+            if not game.OtherActions:
+                game.OtherActions = ObservableCollection[GameAction]()
+            old_action.Name = "Launch without Steam"
+            game.OtherActions.Insert(0, old_action)
+        else:
+            # play_action is already an OtherAction
+            # Just make sure the URL is up to date on the main PlayAction
+            game.PlayAction.Path = steam_URL(shortcut)
+
+    # Save updated shortcuts.vdf
+    try:
+        with open(SHORTCUT_VDF_PATH, "wb") as f:
+            dump_shortcuts(f, steam_shortcuts)
+    except Exception as e:
+        PlayniteApi.Dialogs.ShowErrorMessage(
+            traceback.format_exc(),
+            "Error saving shortcuts.vdf")
+        return
+
+    message = "Please relaunch Steam to update non-Steam shortcuts!\n\n"
+    message += "Updated {} existing non-Steam shortcuts\n".format(games_updated)
+    message += "Created {} new non-Steam shortcuts".format(games_new)
+    if games_skipped:
+        message += "\n\nSkipped {} game(s) without any PlayAction set:\n".format(len(games_skipped))
+        message += "\n".join(games_skipped)
+    if games_url:
+        message += "\n\nWarning: Some games had URL launch actions. (Typically managed by a library plugin.) "
+        message += "You may wish to update their actions and recreate non-Steam shortcuts. "
+        message += "Steam will still launch these games, but the Steam overlay will not function."
+        message += "\n\nThe following {} game(s) had URL launch actions:\n".format(len(games_url))
+        message += "\n".join(games_url)
+    PlayniteApi.Dialogs.ShowMessage(
+        message,
+        "Updated Non-Steam Shortcuts"
+    )
